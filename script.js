3d9f840b87349c4c94ebb51e98814da20fe3b0ac
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-12-10 13:17:24 -0600

    pseudoi coding js file

---
+++ script.js
@@ -0,0 +1,30 @@
+//DOM var
+
+
+//Countdown
+
+
+//Question and answers Array
+
+
+//Start the quiz 
+
+
+//Timer start
+
+
+//Next question
+
+
+//Is it the right the answer
+
+
+
+//Quiz completed/Time is at zero
+
+
+// High score to lowest one
+
+
+// All event listeners
+
