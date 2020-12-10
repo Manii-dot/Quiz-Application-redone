69dee53a6194dadce4dc9e66315e56ddf9c4de20
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-12-10 13:38:11 -0600

    setting up DOM variable

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 //DOM var
+var beginButton = document.getElementById("begin");
+var timerDisplay = document.getElementById("timer");
+var beginCard = document.getElementById("card");
+var theQuestionDiv = document.getElementById("theQuestion");
+var lastPart = document.getElementById("lastPart");
+var theAnswers = document.getElementById("answers");
+var quizScore = document.getElementById("score");
+var submiting = document.getElementById("submit");
+var scoreTab = document.getElementById("scoretable");
+var highScoresTab = document.getElementById("highScores");
+var clearScore = document.getElementById("clear");
+var theScoresLink = document.getElementById("scoreslink");
+var returnButton = document.getElementById("returnBtn");
 
+var bestScoresCache = JSON.parse(localStorage.getItem("bestScoreDisplay")) || [];
 
 //Countdown
 
